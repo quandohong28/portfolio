8890077929da48d20d3216cb0db4b859cfe37509
Đỗ Hồng Quân <quandohong28@192.168.0.102>
2024-09-11 10:36:42 +0700

    home page done

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import avatar from '../../assets/quandohong28.jpeg';
 
 function Home() {
 
     return (
-        <div>Home</div>
+        <>
+            <div className="flex items-center justify-center gap-24 h-[500px]">
+                <div>
+                    <div className='relative flex items-center justify-center'>
+                        <div className='h-60 w-64 bg-orange-300 absolute'></div>
+                        <img className='z-10 border-2 border-black p-4 w-64' src={avatar} alt="avatar" />
+
+                    </div>
+                </div>
+                <div>
+                    <p className='uppercase font-bold text-5xl mb-6'>Full stack <br /> web <br /> developer</p>
+                    <div className='h-200 h-1 bg-black mb-6'></div>
+                    <p className='mb-12'>Professional UI/UX designer
+                        and PHP - Javascript developer</p>
+                    <p className='my-soul-regular text-4xl'>Do Hong Quan</p>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/components/routes/ErrorPage.jsx
+++ src/components/routes/ErrorPage.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function ErrorPage() {
     const error = useRouteError();
 
     return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
+        <div id='error-page' className='flex flex-col items-center justify-center min-h-screen'>
+            <h1 className='text-4xl mb-4'>Oops!</h1>
+            <p className='mb-4'>Sorry, an unexpected error has occurred.</p>
+            <p className='text-xl italic font-bold mb-2'>{error.statusText || error.message}</p>
+            <Link to={`home`} className='text-orange-400'>
+                <FontAwesomeIcon icon={faArrowLeft} className='mr-2' />
+                Back to Home</Link>
         </div>
     )
 }
